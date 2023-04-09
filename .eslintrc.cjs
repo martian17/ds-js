8bfd8d493402ad67124b1a8afd44380649630033
martian17 <yakumodanshi@gmail.com>
2023-04-09 15:10:44 +0900

    Setup eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  "env": {
+    node: true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "env": {
+    "es6": true,
+    "node": true
+  }
+};
